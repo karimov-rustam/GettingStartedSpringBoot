8dd5bc75981c6627dbff8e9e8c5a341a0faf564d
Karimov R <rkarimov.r@gmail.com>
2018-02-09 01:10:36 +0400

    Implement serving static content sample

--- src/main/java/edu/spring/boot/Application.java
+++ src/main/java/edu/spring/boot/Application.java
@@ -3,13 +3,18 @@ package edu.spring.boot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @Controller
-public class Application {
+public class Application extends WebMvcConfigurerAdapter{
+
+    @RequestMapping("/thymeleaf")
+    public String tleaf() {
+        return "template";
+    }
 
     @RequestMapping("/viewresolver")
     public String viewResolver() {
@@ -22,11 +27,11 @@ public class Application {
         return new Cat("Athena", "Bengal", 1);
     }
 
-    @RequestMapping("/mustache")
-    public String mustache(Model model) {
-        model.addAttribute("company", "Roga & Copyta limited");
-        return "template";
-    }
+//    @RequestMapping("/mustache")
+//    public String mustache(Model model) {
+//        model.addAttribute("company", "Roga & Copyta limited");
+//        return "template";
+//    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
