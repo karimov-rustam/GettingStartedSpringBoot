61b5095f32b4a7be934e29e7f13a61f788b9774e
Karimov R <rkarimov.r@gmail.com>
2018-02-09 23:46:46 +0400

    Implement OAuth and form login

--- src/main/java/edu/spring/security/SpringBootSecurityApplication.java
+++ src/main/java/edu/spring/security/SpringBootSecurityApplication.java
@@ -2,9 +2,30 @@ package edu.spring.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @SpringBootApplication
-public class SpringBootSecurityApplication {
+@EnableOAuth2Sso
+@RestController
+public class SpringBootSecurityApplication extends WebSecurityConfigurerAdapter {
+
+    @RequestMapping("/user")
+    public Principal user(Principal principal) {
+        return principal;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests().anyRequest().authenticated().and().formLogin();
+        http.authorizeRequests().anyRequest().authenticated();
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootSecurityApplication.class, args);
