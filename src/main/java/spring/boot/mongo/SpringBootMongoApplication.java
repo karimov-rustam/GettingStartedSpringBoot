18d3795641e1dead2c6f34d27fd27b73245f2971
Karimov R <rkarimov.r@gmail.com>
2018-02-10 17:10:06 +0400

    Add mongo repository support

--- src/main/java/spring/boot/mongo/SpringBootMongoApplication.java
+++ src/main/java/spring/boot/mongo/SpringBootMongoApplication.java
@@ -1,13 +1,8 @@
 package spring.boot.mongo;
 
-import com.mongodb.BasicDBObject;
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
-import com.mongodb.DBObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,19 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class SpringBootMongoApplication {
 
     @Autowired
-    private MongoDbFactory mongo;
+    private StockRepository repo;
 
     @RequestMapping("/stocks")
-    private DBObject stocks() {
-        DB db = mongo.getDb("sandbox");
-        DBCollection collection = db.getCollection("stocks");
+    private Stock stocks() {
+        Stock stock = new Stock();
+        stock.setCompanyName("Tesla");
+        stock.setCeo("Elon Musk");
+        stock.setSymbol("TLSA");
+        stock.setPrice(250.0d);
 
-        collection.insert(
-                new BasicDBObject()
-                        .append("companyName", "Ford")
-                        .append("symbol", "F"));
-
-        return collection.findOne(new BasicDBObject().append("symbol", "F"));
+        repo.save(stock);
+        return repo.findBySymbol("TLSA");
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/spring/boot/mongo/StockRepository.java
@@ -0,0 +1,10 @@
+package spring.boot.mongo;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StockRepository extends MongoRepository<Stock, Integer> {
+
+    public Stock findBySymbol(String symbol);
+}
