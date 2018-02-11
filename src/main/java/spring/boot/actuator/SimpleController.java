69a4281fd0956e46993db060f2bdd9479c32326d
Karimov R <rkarimov.r@gmail.com>
2018-02-12 00:00:37 +0400

    Add endpoints metric

--- src/main/java/spring/boot/actuator/SimpleController.java
+++ src/main/java/spring/boot/actuator/SimpleController.java
@@ -1,13 +1,21 @@
 package spring.boot.actuator;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.actuate.metrics.CounterService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SimpleController {
 
+    @Qualifier("counterService")
+    @Autowired
+    private CounterService counterService;
+
     @RequestMapping("/execute")
     public String execute() {
+        counterService.increment("simple.service.execute");
         return "invoked";
     }
 }
